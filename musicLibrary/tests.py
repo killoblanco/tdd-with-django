8cf62c4ea9ef5b866bea0c90969f8c552a095bd7
Kamilo Vasquez <me@killoblan.co>
2016-12-18 20:49:32 -0500

    Update
    -Adds album, start_time, end_time fields to Solo
    -Updates solo detail page to use new attributes

--- musicLibrary/tests.py
+++ musicLibrary/tests.py
@@ -5,24 +5,28 @@ from solos.models import Solo
 
 
 class StudentTestCase(LiveServerTestCase):
-
     def setUp(self):
         self.browser = webdriver.Chrome('/usr/local/bin/chromedriver')
         self.browser.implicitly_wait(2)
         self.solo1 = Solo.objects.create(
             instrument="saxophone",
             artist="John Coltrane",
-            track="My Favorite Things"
+            track="My Favorite Things",
+            album='My Favorite Things'
         )
         self.solo2 = Solo.objects.create(
             instrument="saxophone",
             artist="Cannonball Adderley",
-            track="All Blues"
+            track="All Blues",
+            album='Kind of Blue',
+            start_time='2:06',
+            end_time='4:01'
         )
         self.solo3 = Solo.objects.create(
             instrument="saxophone",
             artist="Cannonball Adderley",
-            track="Waltz for Debby"
+            track="Waltz for Debby",
+            album='Know What I Mean?'
         )
 
     def find_search_results(self):
@@ -66,14 +70,15 @@ class StudentTestCase(LiveServerTestCase):
         second_search_result[0].click()
         # The solo page has the title, artist and album for
         # this particular solo.
-        import pdb;pdb.set_trace()
-        self.assertEqual(self.browser.current_url, '{}/solos/2'.format(self.live_server_url))
+        # import pdb
+        # pdb.set_trace()
+        self.assertEqual(self.browser.current_url, '{}/solos/2/'.format(self.live_server_url))
         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-artist').text, 'Cannonball Adderley')
         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-album').text, 'Kind of Blue')
         # He also sees the start time and end time of the solo.
         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-start-time').text, '2:06')
         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-end-time').text, '4:01')
-        self.fail('Incomplete Test')
+        # self.fail('Incomplete Test')
 
     def tearDown(self):
         self.browser.quit()
--- solos/tests/test_models.py
+++ solos/tests/test_models.py
@@ -7,11 +7,15 @@ class SoloModelTestCase(TestCase):
         self.solo = Solo.objects.create(
             track="Falling in Love with Love",
             artist="Oscar Peterson",
-            instrument="piano"
+            instrument="piano",
+            album='At the Stratford Shakespearean Festival',
+            start_time='1:24',
+            end_time='4:06'
         )
 
     def test_solo_basic(self):
         """
         Test the basic functionality of Solo
         """
-        self.assertEqual(self.solo.artist, 'Peterson')
+        self.assertEqual(self.solo.artist, 'Oscar Peterson')
+        self.assertEqual(self.solo.end_time, '4:06')
