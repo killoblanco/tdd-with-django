4c59fb486255069d0ba0dc212cdde69e57b23a8e
Kamilo Vasquez <me@killoblan.co>
2016-12-18 19:52:44 -0500

    Adds URL for SoloDetailView

--- solos/tests/test_urls.py
+++ solos/tests/test_urls.py
@@ -12,3 +12,15 @@ class SolosURLsTestCase(TestCase):
         """
         root = resolve('/')
         self.assertEqual(root.func, index)
+
+    def test_solo_details_url(self):
+        """
+        Test that the url for SoloDetail resolve to the
+        correct view function
+        """
+        solo_detail = resolve('/solos/1/')
+        self.assertEqual(
+            solo_detail.func.__name__,
+            'SoloDetailView'
+        )
+        self.assertEqual(solo_detail.kwargs['pk'], 1)
--- solos/urls.py
+++ solos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'/solos/(?P<pk>\d+)/$', views.SoloDetailView.as_view(), name="detail"),
 ]
